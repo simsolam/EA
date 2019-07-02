46d3bbf0b6edceb9cbeb8eab9817a2463492ee83
simsolam <49168585+simsolam@users.noreply.github.com>
2019-07-01 22:04:44 -0500

    Update App.java

--- lab2/lab2A/src/main/java/cs/mum/edu/App.java
+++ lab2/lab2A/src/main/java/cs/mum/edu/App.java
@@ -11,7 +11,7 @@ import java.util.List;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     private static EntityManagerFactory emf;
     public static void main(String[] args) throws Exception {
@@ -25,9 +25,14 @@ public class App
         // save the book
         em.persist(book1);
         // Create new instance of book and set values in it
-        Book book2 = new Book("Fantastic Beasts and Where To Find Them", "123456789", "J.K Rowling",120.0,new SimpleDateFormat("yyyy-MM-dd").parse("2019-11-15"));
+        Book book2 = new Book("Fantastic Beasts and Where To Find Them", "123456789", "J.K Rowling",121.0,new SimpleDateFormat("yyyy-MM-dd").parse("2019-11-15"));
         // save the book
         em.persist(book2);
+
+        // Create new instance of book and set values in it
+        Book book3 = new Book("Lord of the Rings", "123456789", "John R. R. Tolkien",122.0,new SimpleDateFormat("yyyy-MM-dd").parse("2019-11-15"));
+        // save the book
+        em.persist(book3);
 
         em.getTransaction().commit();
         em.close();
@@ -42,6 +47,30 @@ public class App
             System.out.println("name= " + book.getTitle() + ", year= "
                     + book.getPublish_date() + ", author= " + book.getAuthor());
         }
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        //retrieve all books
+        List<Book> bookList1 = em.createQuery("from Book", Book.class).getResultList();
+        // Update any book
+
+        Book bookUpdate = bookList1.get(0);
+        bookUpdate.setTitle("Part 1: "+bookUpdate.getTitle());
+        em.persist(bookUpdate);
+
+        em.remove(bookList.get(1));
+
+        em.getTransaction().commit();
+        em.close();
+
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        // retieve all books
+        em.createQuery("from Book", Book.class).getResultList().forEach(System.out::println);
+
         em.getTransaction().commit();
         em.close();
     }
